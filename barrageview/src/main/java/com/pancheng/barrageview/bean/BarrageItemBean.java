76bca569ef4b9393da16c895d466e32d065f6318
pancheng <pancheng@tencent.com>
2015-11-18 16:41:59 +0800

    update

---
+++ barrageview/src/main/java/com/pancheng/barrageview/bean/BarrageItemBean.java
@@ -0,0 +1,7 @@
+package com.pancheng.barrageview.bean;
+
+/**
+ * Created by pancheng on 2015/11/18.
+ */
+public class BarrageItemBean {
+}
---
+++ barrageview/src/main/java/com/pancheng/barrageview/util/Conifg.java
@@ -0,0 +1,19 @@
+package com.pancheng.barrageview.util;
+
+/**
+ * Created by pancheng on 2015/11/18.
+ */
+public class Conifg {
+
+    /**
+     * Barrage View Config
+     */
+
+
+    /**
+     * Barrage Config
+     */
+
+
+
+}
